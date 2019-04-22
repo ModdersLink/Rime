64e9454daa6ca2ca28695816c5db3587b6cb060c
Orfeas Zafeiris <orfeaz@gmail.com>
2019-04-22 20:17:56 +0300

    Start public interface implementation of the content mounter

---
+++ RimeLib.Content/Mounting/ContentMounter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+using RimeLib.Content.Frostbite.Content;
+using RimeLib.Frostbite;
+using RimeLib.Frostbite.Core;
+using RimeLib.IO;
+
+namespace RimeLib.Content.Mounting
+{
+    public class ContentMounter
+    {
+        public ContentMounter(EngineType p_Engine)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> Mount(string p_GamePath)
+        {
+            return false;
+        }
+
+        public RimeReader GetChunk(GUID p_GUID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public FrostbiteResource GetResource(string p_Path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public RimeReader GetPartition(string p_Path)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
