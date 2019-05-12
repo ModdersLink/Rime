98f60050daf354e5ba14dcbb0c42f24e2c01c11a
Orfeas Zafeiris <orfeaz@gmail.com>
2019-05-12 17:00:21 +0300

    Add serialization support to TableOfContents

--- RimeLib.Content/Frostbite/Storage/Sb/TableOfContents.cs
+++ RimeLib.Content/Frostbite/Storage/Sb/TableOfContents.cs
@@ -16,11 +16,15 @@ namespace RimeLib.Content.Frostbite.Storage.Sb
             if (s_Magic == 0x00CED100 ||
                 s_Magic == 0x01CED100)
             {
+                // Read the signature and enable de-obfuscation by reading the XOR table.
+                var s_Signature = p_Reader.ReadBytes(292);
                 p_Reader.EnableDeobfuscation();
             }
             else if (s_Magic == 0x03CED100) // Signed
             {
-                p_Reader.Seek(0x228, SeekOrigin.Current);
+                // Read the signature and skip the empty XOR table.
+                var s_Signature = p_Reader.ReadBytes(292);
+                p_Reader.Seek(260, SeekOrigin.Current);
             }
             else
             {
@@ -28,6 +32,24 @@ namespace RimeLib.Content.Frostbite.Storage.Sb
             }
 
             (Layout, _) = DbObjectConverter.FromDbObjectReader<T>(p_Reader);
+        }
+
+        public TableOfContents(T p_Layout)
+        {
+            Layout = p_Layout;
+        }
+
+        public bool Serialize(RimeWriter p_Writer)
+        {
+            // Write the header and an empty signature.
+            p_Writer.Write((uint) 0x00CED100);
+            p_Writer.Write(new byte[292]);
+
+            // Enable obfuscation.
+            p_Writer.EnableObfuscation(new byte[260]);
+
+            // Serialize the DbObject.
+            return DbObjectConverter.ToDbObjectWriter(Layout, p_Writer);
         }
     }
 }
