f0dec85c28ab08b5841bf12968426e3fcbbd0a2f
Orfeas Zafeiris <orfeaz@gmail.com>
2019-05-27 11:44:57 +0300

    Fix tests

--- RimeLib.Content.Tests/Content.cs
+++ RimeLib.Content.Tests/Content.cs
@@ -2,6 +2,8 @@ using System;
 using System.Diagnostics;
 using System.IO;
 using RimeLib.Content.Frostbite.Storage.Sb;
+using RimeLib.Content.Mounting;
+using RimeLib.Frostbite;
 using RimeLib.IO;
 using Xunit;
 
@@ -12,9 +14,20 @@ namespace RimeLib.Content.Tests
         [Fact]
         public void Test1()
         {
-            using var s_Reader = new RimeReader(File.Open(@"B:\Games\Battlefield 3\Data\Win32\Chunks0.toc", FileMode.Open));
-            var s_Toc = new TableOfContents<SuperbundleLayout>(s_Reader);
+            using var s_Reader = new RimeReader(File.Open(@"B:\Games\Battlefield 3\Update\Patch\Data\layout_new.toc", FileMode.Open, FileAccess.Read, FileShare.Read));
+            var s_Toc = new TableOfContents<ContentManifest>(s_Reader);
             Debug.WriteLine(s_Toc);
+
+            using var s_Writer = new RimeWriter(File.Open(@"B:\Games\Battlefield 3\Update\Patch\Data\layout_new2.toc", FileMode.Create));
+            s_Toc.Serialize(s_Writer);
+        }
+
+        [Fact]
+        public async void TestMount()
+        {
+            var s_Mounter = new ContentMounter(EngineType.Frostbite2_0);
+
+            Assert.True(await s_Mounter.Mount(@"B:\Games\Battlefield 3\"));
         }
     }
 }
--- RimeLib.Tests/DbObjectConverterTest.cs
+++ RimeLib.Tests/DbObjectConverterTest.cs
@@ -18,15 +18,19 @@ namespace RimeLib.Tests
             if (s_Magic == 0x00CED100 ||
                 s_Magic == 0x01CED100)
             {
+                // Read the signature and enable de-obfuscation by reading the XOR table.
+                var s_Signature = s_Reader.ReadBytes(292);
                 s_Reader.EnableDeobfuscation();
             }
             else if (s_Magic == 0x03CED100) // Signed
             {
-                s_Reader.Seek(0x228, SeekOrigin.Current);
+                // Read the signature and skip the empty XOR table.
+                var s_Signature = s_Reader.ReadBytes(292);
+                s_Reader.Seek(260, SeekOrigin.Current);
             }
             else
             {
-                throw new Exception("The provided DbObject file is invalid.");
+                throw new Exception("Could not find valid header magic for ToC file.");
             }
 
             var s_DbObject = new DbObject(s_Reader);
@@ -52,6 +56,7 @@ namespace RimeLib.Tests
             Test(@"B:\Games\Battlefield 3\Update\Xpack1\Data\Win32\Levels\XP1_002\XP1_002.toc");
             Test(@"B:\Games\Battlefield 3\Update\Xpack4\Data\Win32\Levels\XP4_Rubble\XP4_Rubble.toc");
             Test(@"B:\Games\Battlefield 3\Update\Xpack5\Data\Win32\Levels\XP5_001\XP5_001.toc");
+            Test(@"B:\Games\Battlefield 3\Update\Patch\Data\Win32\Levels\XP3_Valley\XP3_Valley.toc");
         }
     }
 }
