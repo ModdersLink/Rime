cff1f44ae5f8f1df2d8af50c79e21e0d8f9b8012
Orfeas Zafeiris <orfeaz@gmail.com>
2019-10-06 19:47:10 +0300

    Update the zlib rime reader to use the new stream interface

--- RimeLibLite/IO/ZlibRimeReader.cs
+++ RimeLibLite/IO/ZlibRimeReader.cs
@@ -1,11 +1,14 @@
-﻿using System.IO;
-using ICSharpCode.SharpZipLib;
+﻿using System;
+using System.Diagnostics;
+using System.IO;
 using ICSharpCode.SharpZipLib.Zip.Compression.Streams;
 
 namespace RimeLib.IO
 {
     public class ZlibRimeReader : RimeReader
     {
+        public override bool CanSeek => false;
+
         public long? OriginalSize => m_OriginalSize;
 
         protected long? m_OriginalSize;
@@ -32,7 +35,6 @@ namespace RimeLib.IO
         public override void Dispose()
         {
             base.Dispose();
-
             m_DeflateStream.Dispose();
         }
 
@@ -42,56 +44,15 @@ namespace RimeLib.IO
             m_CompressedSize = ((RimeReader) BaseStream).ReadUInt32();
         }
 
+        public override long Seek(long p_Offset, SeekOrigin p_Origin)
+        {
+            throw new NotSupportedException();
+        }
+
         protected override int ReadInternal(byte[] p_Data, int p_Index, int p_Count)
         {
+            Debug.WriteLine($"Zlib reader with a compressed size of {m_CompressedSize} reading {p_Count} bytes.");
             return m_DeflateStream.Read(p_Data, p_Index, p_Count);
-            /*// User has requested p_Count bytes. Read as much from the base reader.
-            var s_ReadBytes = 0;
-
-            while (s_ReadBytes < p_Count)
-            {
-                // Now, the problem is that in cases where we don't know the size of the compressed stream.
-                // In these cases we want to be a bit conservative and feed data into the deflate stream one
-                // byte at time. This will be slow, obviously, but it's the only safe way.
-                long s_BytesToRead = 1;
-                var s_BytesLeft = p_Count - s_ReadBytes;
-
-                if (m_CompressedSize != null)
-                {
-                    // Check if we have enough data to read.
-                    var s_CurrentPosition = BaseStream.Position - m_StartPosition;
-
-                    if (s_CurrentPosition >= m_CompressedSize)
-                        throw new EndOfStreamException("End of stream reached with " + s_BytesLeft + " bytes left to read.");
-                
-                    var s_RemainingBytes = m_CompressedSize.Value - s_CurrentPosition;
-                    s_BytesToRead = System.Math.Min(s_RemainingBytes, p_Count);
-                }
-
-                // Feed some data to the deflate stream.
-                m_MemoryStream.Write(m_BaseReader.ReadBytes((int) s_BytesToRead));
-                m_MemoryStream.Flush();
-                m_MemoryStream.Seek(-s_BytesToRead, SeekOrigin.Current);
-
-                // Read as many data as we have left.
-                try
-                {
-                    s_ReadBytes += m_DeflateStream.Read(p_Data, p_Index + s_ReadBytes, s_BytesLeft);
-                }
-                catch (SharpZipBaseException s_Exception)
-                {
-                    // If we've hit an EOF just continue feeding data to the deflate stream.
-                    if (s_Exception.Message == "Unexpected EOF")
-                    {
-                        
-                        continue;
-                    }
-
-                    throw;
-                }
-            }
-
-            return p_Count;*/
         }
     }
 }
