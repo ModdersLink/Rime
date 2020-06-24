683e565b4b96317b00ed1f16475fe1c6fecf9e07
kiwidoggie <forums@allenthinks.com>
2020-06-24 08:01:38 -0700

    Add skeleton extraction

--- Utils/MeshExtractor/FbExtensions.cs
+++ Utils/MeshExtractor/FbExtensions.cs
@@ -1,8 +1,6 @@
 ﻿using RimeLib.Frostbite.Core;
-using System;
-using System.Collections.Generic;
+using RimeLib.Serialization.Containers;
 using System.Numerics;
-using System.Text;
 
 namespace MeshExtractor
 {
@@ -15,6 +13,15 @@ namespace MeshExtractor
                 p_Matrix.Y.X, p_Matrix.Y.Y, p_Matrix.Y.Z, p_Matrix.Y.W,
                 p_Matrix.Z.X, p_Matrix.Z.Y, p_Matrix.Z.Z, p_Matrix.Z.W,
                 p_Matrix.W.X, p_Matrix.W.Y, p_Matrix.W.Z, p_Matrix.W.W);
+        }
+
+        public static Matrix4x4 ToMatrix4x4 (this LinearTransform p_Transform)
+        {
+            return new Matrix4x4(
+                p_Transform.Right.X, p_Transform.Right.Y, p_Transform.Right.Z, 0,
+                p_Transform.Up.X, p_Transform.Up.Y, p_Transform.Up.Z, 0,
+                p_Transform.Forward.X, p_Transform.Forward.Y, p_Transform.Forward.Z, 0,
+                p_Transform.Trans.X, p_Transform.Trans.Y, p_Transform.Trans.Z, 0);
         }
     }
 }
