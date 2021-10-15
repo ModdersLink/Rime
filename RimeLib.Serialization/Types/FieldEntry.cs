bafdfd0637d12e0d5734a40758d1c447bc7de9dc
kiwidoggie <forums@allenthinks.com>
2021-10-15 05:35:36 -0700

    FieldEntry work cont.

--- RimeLib.Serialization/Types/FieldEntry.cs
+++ RimeLib.Serialization/Types/FieldEntry.cs
@@ -8,18 +8,29 @@ namespace RimeLib.Serialization.Types
 {
     public class FieldEntry
     {
+        // The type of the underlying data
         private Type m_Type;
-        private string m_Name;
-        private int m_OffsetInPayload;
-        private dynamic m_Value;
 
+        // Field name
+        private string m_Name;
+
+        // Offset in the payload
+        private int m_OffsetInPayload;
+
+
+        // Rime specific things
         private TypeEntry m_Parent;
+
+        // MemberInfoFlags from frostbite
+        private ushort m_FrostbiteFlags;
+
+        public bool IsBlittable => m_FrostbiteFlags != 0; // TODO: Fix this
 
         public FieldEntry(TypeEntry p_ParentEntry, dynamic p_Value)
         {
             m_Parent = p_ParentEntry;
             m_Name = string.Empty;
-            m_Value = p_Value;
+            m_Type = p_Value.GetType();
         }
     }
 }
