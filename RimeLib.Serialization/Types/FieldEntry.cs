9024eb8ae72f623a4394f3333f71f01995314380
kiwidoggie <forums@allenthinks.com>
2021-10-14 20:49:46 -0700

    Begin implementation

---
+++ RimeLib.Serialization/Types/FieldEntry.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RimeLib.Serialization.Types
+{
+    public class FieldEntry
+    {
+        private Type m_Type;
+        private string m_Name;
+        private int m_OffsetInPayload;
+        private dynamic m_Value;
+
+        private TypeEntry m_Parent;
+
+        public FieldEntry(TypeEntry p_ParentEntry, dynamic p_Value)
+        {
+            m_Parent = p_ParentEntry;
+            m_Name = string.Empty;
+            m_Value = p_Value;
+        }
+    }
+}
---
+++ RimeLib.Serialization/Types/TypeEntry.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RimeLib.Serialization.Types
+{
+    public class TypeEntry
+    {
+        private TypeEntry m_InheritedType;
+        private int m_Alignment;
+        private int m_TypeSize;
+        private List<FieldEntry> m_Fields;
+
+        public TypeEntry()
+        {
+            m_Fields = new List<FieldEntry>();
+        }
+    }
+}
