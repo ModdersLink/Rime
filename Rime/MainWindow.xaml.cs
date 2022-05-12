a37f6f04a5a4b31be9b5199b5b641be6ef392dc4
OrfeasZ <orfeaz@gmail.com>
2022-05-12 13:28:13 +0300

    Trying to make sense of shaders

--- Rime/MainWindow.xaml.cs
+++ Rime/MainWindow.xaml.cs
@@ -1,10 +1,18 @@
 ﻿using System;
+using System.IO;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Media;
 using System.Windows.Threading;
+using fb;
 using RimeLib;
 using RimeLib.Content.Mounting;
+using RimeLib.Extensions;
 using RimeLib.Frostbite;
+using RimeLib.IO;
+using RimeLib.Mesh.Frostbite;
+using RimeLib.Mesh.Frostbite2_0;
+using RimeLib.Shader.Frostbite2_0.Frostbite;
 using RimeLib.Texture;
 using RimeLib.Utils;
 
@@ -23,24 +31,53 @@ namespace Rime
 
         private void OnRendererStarted(object? p_Sender, EventArgs p_E)
         {
+            MountAndSetup().Wait();
+        }
+
+        private async Task MountAndSetup()
+        {
             AssemblyUtils.LoadSupportAssembly(AssemblyType.Content, EngineType.Frostbite2_0);
             AssemblyUtils.LoadSupportAssembly(AssemblyType.Serialization, EngineType.Frostbite2_0);
             AssemblyUtils.LoadSupportAssembly(AssemblyType.Texture, EngineType.Frostbite2_0);
 
             var s_Mounter = EngineInterfaceRegistry.Create<IEngineMounter>(EngineType.Frostbite2_0);
 
-            s_Mounter.Mount("B:\\Games\\Battlefield 3", true, EngineType.Frostbite2_0);
+            await s_Mounter.Mount("B:\\Games\\Battlefield 3", false, EngineType.Frostbite2_0);
+            await s_Mounter.MountSuperbundle("Win32/Chunks0", true);
+            await s_Mounter.MountSuperbundle("Win32/Chunks1", true);
+            await s_Mounter.MountSuperbundle("Win32/Chunks2", true);
+            await s_Mounter.MountSuperbundle("Win32/MpChunks", true);
+            await s_Mounter.MountSuperbundle("Win32/Xp2Chunks", true);
+            await s_Mounter.MountSuperbundle("Win32/Levels/XP2_Skybar/XP2_Skybar", true);
 
-            if (!s_Mounter.TryGetResource(
-                    "weapons/xp2_knife_razorblade/knife_razorblade_d",
-                    out var s_Resource
-                ))
+            if (!s_Mounter.TryGetResource("weapons/xp2_knife_razorblade/knife_razorblade_d", out var s_TextureResource))
                 return;
 
             var s_Converter = EngineInterfaceRegistry.Create<ITextureConverter>(EngineType.Frostbite2_0);
-            var s_Texture = s_Converter.CreateTextureResourceView(s_Resource.FirstVariant, s_Mounter, Renderer.RenderManager.Device);
+            var s_Texture = s_Converter.CreateTextureResourceView(s_TextureResource.FirstVariant, s_Mounter, Renderer.RenderManager.Device);
 
             Renderer.RenderManager.SetTexture(s_Texture);
+            
+            if (!s_Mounter.TryGetResource("xp2/objects/wallmodulesskybar/wallsskybar64_shelf_mesh", out var s_MeshResource))
+                return;
+            
+            if (!s_Mounter.TryGetResource("levels/xp2_skybar/xp2_skybar/shaderdb", out var s_ShaderDbResource))
+                return;
+
+            using var s_ShaderReader = s_ShaderDbResource.FirstVariant.GetReader();
+            var s_ShaderData = s_ShaderReader.ToArray();
+            using var s_TempShaderReader = new RimeReader(new MemoryStream(s_ShaderData));
+            var s_ShaderDb = new ShaderDatabaseContainer(s_TempShaderReader, s_Mounter);
+
+            using var s_MeshReader = s_MeshResource.FirstVariant.GetReader();
+            var s_MeshData = s_MeshReader.ToArray();
+            using var s_TempMeshReader = new RimeReader(new MemoryStream(s_MeshData));
+            var s_MeshSet = new MeshSetLayout(s_TempMeshReader);
+
+            var s_Shader = s_ShaderDb.Databases[ShaderRenderPath.ShaderRenderPath_Dx11]
+                .Shaders["XP2/Objects/WallModulesSkybar/Stone_01"];
+
+            Renderer.RenderManager.DrawMesh(s_MeshSet, s_Shader, s_Mounter);
         }
 
         private void Window_Loaded(object p_Sender, RoutedEventArgs p_Event)
--- Rime/RimeRenderManager.cs
+++ Rime/RimeRenderManager.cs
@@ -1,8 +1,17 @@
 ﻿using System;
-using System.Numerics;
+using System.Collections.Generic;
+using System.Linq;
 using GameToolsDotNet.Rendering.DirectX;
 using ImGuiNET;
+using RimeLib.Content.Mounting;
+using RimeLib.Mesh.Frostbite;
+using RimeLib.Shader.Frostbite2_0.Frostbite;
+using SharpDX;
 using SharpDX.Direct3D11;
+using SharpDX.DXGI;
+using Buffer = SharpDX.Direct3D11.Buffer;
+using Resource = SharpDX.Direct3D11.Resource;
+using Vector2 = System.Numerics.Vector2;
 
 namespace Rime;
 
@@ -56,5 +65,200 @@ public class RimeRenderManager : RenderManager
 
             ImGui.End();
         }
+    }
+
+    private MeshSetLayout? m_MeshSet;
+    private SurfaceShaderInfo? m_Shader;
+    private Buffer? m_IndexBuffer;
+    private Buffer[] m_VertexBuffers = Array.Empty<Buffer>();
+    private VertexBufferBinding[] m_VertexBufferBindings = Array.Empty<VertexBufferBinding>();
+
+    private VertexShader[] m_VertexShaders;
+    private InputLayout[] m_InputLayouts;
+    private SamplerState[][] m_VertexSamplers;
+    private PixelShader[] m_PixelShaders;
+    private SamplerState[][] m_PixelSamplers;
+    private GeometryShader[] m_GeometryShaders;
+
+    public void DrawMesh(MeshSetLayout p_MeshSet, SurfaceShaderInfo p_Shader, IEngineMounter p_Mounter)
+    {
+        var s_LodPtr = p_MeshSet.Lods.First();
+        
+        //foreach (var s_LodPtr in p_MeshSet.Lods)
+        {
+            var s_Lod = s_LodPtr.Object;
+            
+            var s_Name = s_Lod.Name.Object;
+
+            var s_LodChunkId = s_Lod.DataChunkId;
+            
+            if (!p_Mounter.TryGetChunk(s_LodChunkId, out var s_Chunk))
+                return;
+
+            using var s_LodReader = s_Chunk.FirstVariant.GetReader();
+
+            if (s_Lod.IndexBufferFormat == IndexBufferFormat.IndexBufferFormat_16Bit)
+            {
+                var s_Indices = new ushort[s_Lod.IndexDataSize / sizeof(ushort)];
+
+                for (var i = 0; i < s_Indices.Length; ++i)
+                    s_Indices[i] = s_LodReader.ReadUInt16();
+                
+                m_IndexBuffer = Buffer.Create(Device, BindFlags.IndexBuffer, s_Indices);
+            }
+            else
+            {
+                var s_Indices = new uint[s_Lod.IndexDataSize / sizeof(uint)];
+
+                for (var i = 0; i < s_Indices.Length; ++i)
+                    s_Indices[i] = s_LodReader.ReadUInt32();
+                
+                m_IndexBuffer = Buffer.Create(Device, BindFlags.IndexBuffer, s_Indices);
+            }
+
+            m_VertexBuffers = new Buffer[s_Lod.Subsets.Count];
+            m_VertexBufferBindings = new VertexBufferBinding[s_Lod.Subsets.Count];
+
+            for (var i = 0; i < s_Lod.Subsets.Get.Length; i++)
+            {
+                var s_Subset = s_Lod.Subsets.Get[i];
+                var s_VertexBufferSize = (int)(s_Subset.VertexCount * s_Subset.VertexStride);
+                
+                using var s_VertexStream = new DataStream(
+                    s_VertexBufferSize,
+                    true,
+                    true
+                );
+
+                s_VertexStream.Write(s_LodReader.ReadBytes(s_VertexBufferSize), 0, s_VertexBufferSize);
+
+                m_VertexBuffers[i] = new Buffer(
+                    Device,
+                    s_VertexStream,
+                    s_VertexBufferSize,
+                    ResourceUsage.Default,
+                    BindFlags.VertexBuffer,
+                    CpuAccessFlags.None,
+                    ResourceOptionFlags.None,
+                    0
+                );
+
+                m_VertexBufferBindings[i] = new VertexBufferBinding(m_VertexBuffers[i], s_Subset.VertexStride, 0);
+            }
+
+            var s_VertexShaders = new List<VertexShader>();
+            var s_InputLayouts = new List<InputLayout>();
+            var s_VertexSamplers = new List<SamplerState[]>();
+            var s_PixelShaders = new List<PixelShader>();
+            var s_PixelSamplers = new List<SamplerState[]>();
+            var s_GeometryShaders = new List<GeometryShader>();
+            
+            foreach (var s_Solution in p_Shader.Solutions)
+            {
+                if (s_Solution.VertexPermutation != null)
+                {
+                    var s_Shader = new VertexShader(Device, s_Solution.VertexPermutation.ShaderBytecode);
+                    var s_InputLayout = new InputLayout(
+                        Device,
+                        s_Solution.VertexPermutation.InputSignatureBytecode,
+                        s_Solution.VertexPermutation.Elements
+                    );
+
+                    s_VertexShaders.Add(s_Shader);
+                    s_InputLayouts.Add(s_InputLayout);
+                }
+
+                if (s_Solution.VertexConstants != null)
+                {
+                    var s_Samplers = new List<SamplerState>();
+                    
+                    foreach (var s_Sampler in s_Solution.VertexConstants.Samplers)
+                    {
+                        s_Samplers.Add(new SamplerState(Device, s_Sampler.Desc));
+                    }
+                    
+                    s_VertexSamplers.Add(s_Samplers.ToArray());
+                }
+                
+                if (s_Solution.PixelPermutation != null)
+                {
+                    var s_Shader = new PixelShader(Device, s_Solution.PixelPermutation.ShaderBytecode);
+                   
+                    s_PixelShaders.Add(s_Shader);
+                }
+
+                if (s_Solution.PixelConstants != null)
+                {
+                    var s_Samplers = new List<SamplerState>();
+                    
+                    foreach (var s_Sampler in s_Solution.PixelConstants.Samplers)
+                    {
+                        s_Samplers.Add(new SamplerState(Device, s_Sampler.Desc));
+                    }
+                    
+                    s_PixelSamplers.Add(s_Samplers.ToArray());
+                }
+                
+                if (s_Solution.GeometryPermutation != null)
+                {
+                    var s_Shader = new GeometryShader(Device, s_Solution.GeometryPermutation.ShaderBytecode);
+                   
+                    s_GeometryShaders.Add(s_Shader);
+                }
+            }
+            
+            m_VertexShaders = s_VertexShaders.ToArray();
+            m_InputLayouts = s_InputLayouts.ToArray();
+            m_VertexSamplers = s_VertexSamplers.ToArray();
+            m_PixelShaders = s_PixelShaders.ToArray();
+            m_PixelSamplers = s_PixelSamplers.ToArray();
+            m_GeometryShaders = s_GeometryShaders.ToArray();
+        }
+
+        m_Shader = p_Shader;
+        m_MeshSet = p_MeshSet;
+    }
+
+    protected override void DrawFrameEarly(bool p_IsFocused)
+    {
+        base.DrawFrameEarly(p_IsFocused);
+
+        if (m_MeshSet == null)
+            return;
+
+        var s_Lod = m_MeshSet.Lods.First().Object;
+
+        // Set input assembler index buffer.
+        var s_Format = s_Lod.IndexBufferFormat == IndexBufferFormat.IndexBufferFormat_16Bit
+            ? Format.R16_UInt
+            : Format.R32_UInt;
+        
+        Device.ImmediateContext.InputAssembler.SetIndexBuffer(m_IndexBuffer, s_Format, 0);
+        
+        // NOTE: For each solution create vertex shader, input elements,
+        // use them to create an input layout and a pixel shader,
+        // and to also create pixel samplers.
+
+        // Store rasterizer state.
+        var s_PrevState = Device.ImmediateContext.Rasterizer.State;
+
+        Device.ImmediateContext.InputAssembler.SetVertexBuffers(0, m_VertexBufferBindings);
+        
+        for (var i = 0; i < s_Lod.Subsets.Count; ++i)
+        {
+            // Set vertex shader.
+            
+            // Set input assembler input layout.
+            // Set pixel shader.
+            // Set pixel shader samplers.
+            // Set input assembler primitive topology.
+            // Set input assembler vertex buffer.
+            // Set pixel shader shader resources.
+            // Set pixel shader constant buffer.
+            // Draw indexed.
+        }
+
+        // Restore rasterizer state.
+        Device.ImmediateContext.Rasterizer.State = s_PrevState;
     }
 }
