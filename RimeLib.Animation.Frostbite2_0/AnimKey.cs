ba5b7f9aa58e07e7aa7a83dfaecc8925d5660949
FlashHit <56718716+FlashHit@users.noreply.github.com>
2022-10-09 20:19:54 +0200

    Support AnimTrackData

---
+++ RimeLib.Animation.Frostbite2_0/AnimKey.cs
@@ -0,0 +1,69 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using RimeLib.Frostbite;
+using RimeLib.IO;
+
+namespace RimeLib.Animation.Frostbite2_0;
+
+public class AnimKey : IFbSerializable
+{
+    public float Time { get; set; }
+    public float Value { get; set; }
+    public float InTanX { get; set; }
+    public float InTanY { get; set; }
+    public float OutTanX { get; set; }
+    public float OutTanY { get; set; }
+
+    public AnimKey()
+    {
+
+    }
+
+    public AnimKey(RimeReader p_Reader)
+    {
+        Deserialize(p_Reader);
+    }
+
+    public bool Serialize(RimeWriter p_Writer)
+    {
+        p_Writer.Write(Time);
+        p_Writer.Write(Value);
+        p_Writer.Write(InTanX);
+        p_Writer.Write(InTanY);
+        p_Writer.Write(OutTanX);
+        p_Writer.Write(OutTanY);
+
+        return true;
+    }
+
+    public bool Serialize([NotNullWhen(true)] out byte[]? p_Data)
+    {
+        var s_Stream = new MemoryStream();
+        using var s_Writer = new RimeWriter(s_Stream);
+
+        if (Serialize(s_Writer))
+        {
+            p_Data = s_Stream.ToArray();
+            return true;
+        }
+
+        p_Data = null;
+        return false;
+    }
+
+    public void Deserialize(RimeReader p_Reader)
+    {
+        Time = p_Reader.ReadSingle();
+        Value = p_Reader.ReadSingle();
+        InTanX = p_Reader.ReadSingle();
+        InTanY = p_Reader.ReadSingle();
+        OutTanX = p_Reader.ReadSingle();
+        OutTanY = p_Reader.ReadSingle();
+    }
+
+    public void Deserialize(byte[] p_Data)
+    {
+        using var s_Reader = new RimeReader(new MemoryStream(p_Data));
+        Deserialize(s_Reader);
+    }
+}
---
+++ RimeLib.Animation.Frostbite2_0/AnimTrackData.cs
@@ -0,0 +1,58 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using RimeLib.Frostbite;
+using RimeLib.IO;
+
+namespace RimeLib.Animation.Frostbite2_0.Frostbite;
+
+public class AnimTrackData : IFbSerializable
+{
+    public AnimKey[] AnimKeys { get; set; }
+
+    public AnimTrackData()
+    {
+
+    }
+
+    public AnimTrackData(RimeReader p_Reader)
+    {
+        Deserialize(p_Reader);
+    }
+
+    public bool Serialize(RimeWriter p_Writer)
+    {
+        foreach (var s_AnimKey in AnimKeys)
+        {
+            s_AnimKey.Serialize(p_Writer);
+        }
+
+        return true;
+    }
+
+    public bool Serialize([NotNullWhen(true)] out byte[]? p_Data)
+    {
+        var s_Stream = new MemoryStream();
+        using var s_Writer = new RimeWriter(s_Stream);
+
+        if (Serialize(s_Writer))
+        {
+            p_Data = s_Stream.ToArray();
+            return true;
+        }
+
+        p_Data = null;
+        return false;
+    }
+
+    public void Deserialize(RimeReader p_Reader)
+    {
+        for (var i = 0; i < p_Reader.Length; i++)
+            AnimKeys.SetValue(new AnimKey(p_Reader), i);
+    }
+
+    public void Deserialize(byte[] p_Data)
+    {
+        using var s_Reader = new RimeReader(new MemoryStream(p_Data));
+        Deserialize(s_Reader);
+    }
+}
