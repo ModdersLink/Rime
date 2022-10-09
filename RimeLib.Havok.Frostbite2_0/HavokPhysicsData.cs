06d30b9896efd8c6702679fdb3c49c0d04071168
kiwidoggie <forums@allenthinks.com>
2022-10-08 18:56:56 -0700

    Rewrite HavokPhysicsData to avoid RelocArray/Ptr

--- RimeLib.Havok.Frostbite2_0/HavokPhysicsData.cs
+++ RimeLib.Havok.Frostbite2_0/HavokPhysicsData.cs
@@ -10,10 +10,15 @@ namespace RimeLib.Havok.Frostbite2_0;
 public class HavokPhysicsData : IFbSerializable
 {
     public uint PartCount { get; set; }
-    public RelocArray<Vec3> PartTranslations { get; set; } = new RelocArray<Vec3>();
-    public RelocArray<AxisAlignedBox> LocalAabbs { get; set; } = new RelocArray<AxisAlignedBox>();
-    public RelocArray<byte> MaterialIndices { get; set; } = new RelocArray<byte>();
-    public RelocArray<uint> MaterialFlagsAndIndices { get; set; } = new RelocArray<uint>();
+    // public RelocArray<Vec3> PartTranslations { get; set; } = new RelocArray<Vec3>();
+    public List<Vec3> PartTranslations { get; set; } = new();
+    // TODO: Wait for RelocArray/Ptr to be unfucked, manually parse for now
+    // public RelocArray<AxisAlignedBox> LocalAabbs { get; set; } = new RelocArray<AxisAlignedBox>();
+    public List<AxisAlignedBox> LocalAabbs { get; set; } = new();
+    // public RelocArray<byte> MaterialIndices { get; set; } = new RelocArray<byte>();
+    public List<byte> MaterialIndices { get; set; } = new();
+    // public RelocArray<uint> MaterialFlagsAndIndices { get; set; } = new RelocArray<uint>();
+    public List<uint> MaterialFlagsAndIndices { get; set; } = new();
     public float Scale { get; set; }
     public byte MaterialCountUsed { get; set; }
     public byte HighestMaterialIndex { get; set; }
@@ -36,17 +41,19 @@ public class HavokPhysicsData : IFbSerializable
 
     public bool Serialize(RimeWriter p_Writer)
     {
-        p_Writer.Write(PartCount);
-        p_Writer.Write(PartTranslations.BaseAddress);
-        p_Writer.Write(LocalAabbs.BaseAddress);
-        p_Writer.Write(MaterialIndices.BaseAddress);
-        p_Writer.Write(MaterialFlagsAndIndices.BaseAddress);
-        p_Writer.Write(Scale);
-        p_Writer.Write(MaterialCountUsed);
-        p_Writer.Write(HighestMaterialIndex);
-        p_Writer.Write(Padding);
+        // TODO: Pending re-write of relocptr/array
+        throw new NotImplementedException();
+        //p_Writer.Write(PartCount);
+        //p_Writer.Write(PartTranslations.BaseAddress);
+        //p_Writer.Write(LocalAabbs.BaseAddress);
+        //p_Writer.Write(MaterialIndices.BaseAddress);
+        //p_Writer.Write(MaterialFlagsAndIndices.BaseAddress);
+        //p_Writer.Write(Scale);
+        //p_Writer.Write(MaterialCountUsed);
+        //p_Writer.Write(HighestMaterialIndex);
+        //p_Writer.Write(Padding);
 
-        return true;
+        //return true;
     }
 
     public bool Serialize([NotNullWhen(true)] out byte[]? p_Data)
@@ -69,10 +76,84 @@ public class HavokPhysicsData : IFbSerializable
     public void Deserialize(RimeReader p_Reader)
     {
         PartCount = p_Reader.ReadUInt32();
-        PartTranslations = new RelocArray<Vec3>(p_Reader);
-        LocalAabbs = new RelocArray<AxisAlignedBox>(p_Reader);
-        MaterialIndices = new RelocArray<byte>(p_Reader);
-        MaterialFlagsAndIndices = new RelocArray<uint>(p_Reader);
+
+        // Manually parse this out until we can fix the hack
+        var s_PartTranslationsCount = p_Reader.ReadUInt32();
+        var s_PartTranslationsOffset = p_Reader.ReadInt64();
+
+        var s_Position = p_Reader.Position;
+        p_Reader.Seek(s_PartTranslationsOffset, SeekOrigin.Begin);
+        for (var i = 0; i < s_PartTranslationsCount; i++)
+        {
+            PartTranslations.Add(new Vec3
+            {
+                x = p_Reader.ReadSingle(),
+                y = p_Reader.ReadSingle(),
+                z = p_Reader.ReadSingle(),
+            });
+            // Vec3 has padding
+            p_Reader.ReadSingle();
+        }
+        p_Reader.Seek(s_Position, SeekOrigin.Begin);
+
+        // PartTranslations = new RelocArray<Vec3>(p_Reader);
+
+        var s_LocalAabbsCount = p_Reader.ReadUInt32();
+        var s_LocalAabbsOffset = p_Reader.ReadInt64();
+
+        s_Position = p_Reader.Position;
+        p_Reader.Seek(s_LocalAabbsOffset, SeekOrigin.Begin);
+        for (var i = 0; i < s_LocalAabbsCount; i++)
+        {
+            var s_Min = new Vec3
+            {
+                x = p_Reader.ReadSingle(),
+                y = p_Reader.ReadSingle(),
+                z = p_Reader.ReadSingle()
+            };
+            p_Reader.ReadSingle();
+
+            var s_Max = new Vec3
+            {
+                x = p_Reader.ReadSingle(),
+                y = p_Reader.ReadSingle(),
+                z = p_Reader.ReadSingle()
+            };
+            p_Reader.ReadSingle();
+
+            LocalAabbs.Add(new AxisAlignedBox
+            {
+                min = s_Min,
+                max = s_Max,
+            });
+        }
+        p_Reader.Seek(s_Position,SeekOrigin.Begin);
+
+        // LocalAabbs = new RelocArray<AxisAlignedBox>(p_Reader);
+
+        var s_MaterialIndicesCount = p_Reader.ReadUInt32();
+        var s_MaterialIndicesOffset = p_Reader.ReadInt64();
+
+        s_Position = p_Reader.Position;
+        p_Reader.Seek(s_MaterialIndicesOffset, SeekOrigin.Begin);
+        for (var i = 0; i < s_MaterialIndicesCount; i++)
+            MaterialIndices.Add(p_Reader.ReadUByte());
+
+        p_Reader.Seek(s_Position, SeekOrigin.Begin);
+
+        // MaterialIndices = new RelocArray<byte>(p_Reader);
+
+        var s_MaterialFlagAndIndicesCount = p_Reader.ReadUInt32();
+        var s_MateiralFlagAndIndciesOffset = p_Reader.ReadInt64();
+        s_Position = p_Reader.Position;
+        p_Reader.Seek(s_MateiralFlagAndIndciesOffset, SeekOrigin.Begin);
+        for (var i = 0; i < s_MaterialFlagAndIndicesCount; ++i)
+            MaterialFlagsAndIndices.Add(p_Reader.ReadUInt32());
+
+        p_Reader.Seek(s_Position, SeekOrigin.Begin);
+
+        // MaterialFlagsAndIndices = new RelocArray<uint>(p_Reader);
+
         Scale = p_Reader.ReadSingle();
         MaterialCountUsed = p_Reader.ReadUByte();
         HighestMaterialIndex = p_Reader.ReadUByte();
