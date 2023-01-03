ad9d8ff3f7272f142bc332cbfff1981217636ae9
kiwidoggie <forums@allenthinks.com>
2023-01-03 11:02:08 -0800

    Add animation class

--- RimeLib.Animation/Animation.cs
+++ RimeLib.Animation/Animation.cs
@@ -1,6 +1,52 @@
-﻿namespace RimeLib.Animation
+﻿
+using RimeLib.Frostbite;
+using RimeLib.Frostbite.Core;
+using RimeLib.IO;
+using System.Diagnostics.CodeAnalysis;
+
+namespace RimeLib.Animation
 {
-    public class Animation
+    /// <summary>
+    /// Credits: IceBloc (https://github.com/marv7000/IceBloc)
+    /// 
+    /// </summary>
+    public class Animation : IFbSerializable
     {
+        public int CodecType { get; set; }
+        public int AnimId { get; set; }
+        public float TrimOffset { get; set; }
+        public ushort EndFrame { get; set; }
+        public bool Additive { get; set; }
+        public GUID ChannelToDofAsset { get; set; } = GUID.Empty;
+        
+        public Animation() 
+        { 
+
+        }
+
+        public Animation(RimeReader p_Reader)
+        {
+
+        }
+
+        public bool Serialize(RimeWriter p_Writer)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Serialize([NotNullWhen(true)] out byte[]? p_Data)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Deserialize(RimeReader p_Reader)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Deserialize(byte[] p_Data)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
