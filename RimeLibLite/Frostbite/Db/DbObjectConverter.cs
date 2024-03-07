dd5d3d032ceaa652e5c8b5253da9cc9bd89cfe2c
txt231 <4959042+txt231@users.noreply.github.com>
2024-03-07 22:27:42 +0100

    DbObject: add basic support for ulong and uint in structures

--- RimeLibLite/Frostbite/Db/DbObjectConverter.cs
+++ RimeLibLite/Frostbite/Db/DbObjectConverter.cs
@@ -279,6 +279,12 @@ namespace RimeLib.Frostbite.Db
                 EnsureElementType(p_Element, DbObjectType.Integer);
                 return p_Element.Value;
             }
+            if (s_FieldType == typeof(uint) || s_FieldType == typeof(uint?))
+            {
+                EnsureElementType(p_Element, DbObjectType.Integer);
+                return (uint)p_Element.Value;
+            }
+
 
             if (s_FieldType == typeof(long) || s_FieldType == typeof(long?))
             {
@@ -288,6 +294,15 @@ namespace RimeLib.Frostbite.Db
                     return null;
 
                 return Convert.ToInt64(p_Element.Value);
+            }
+            if (s_FieldType == typeof(ulong) || s_FieldType == typeof(ulong?))
+            {
+                EnsureElementType(p_Element, DbObjectType.Integer, DbObjectType.Long, DbObjectType.VarInt);
+
+                if (p_Element.Value == null)
+                    return null;
+
+                return Convert.ToUInt64(p_Element.Value);
             }
 
             if (s_FieldType == typeof(float) || s_FieldType == typeof(float?))
@@ -389,6 +404,8 @@ namespace RimeLib.Frostbite.Db
                 return new DbObjectElement(p_FieldName, (int) p_Object);
 
             if (s_Type == typeof(long) || s_Type == typeof(long?))
+                return new DbObjectElement(p_FieldName, (long) p_Object, p_VariableLength);
+            if (s_Type == typeof(ulong) || s_Type == typeof(ulong?))
                 return new DbObjectElement(p_FieldName, (long) p_Object, p_VariableLength);
 
             if (s_Type == typeof(float) || s_Type == typeof(float?))
