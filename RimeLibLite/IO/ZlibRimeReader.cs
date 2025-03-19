632bf51e383feb97e6169e91ba9a9938c92cec4f
txt231 <4959042+txt231@users.noreply.github.com>
2025-03-19 22:02:24 +0100

    ZlibRimeReader: add experimental seeking, and set buffer sizes to match data sizes

--- RimeLibLite/IO/ZlibRimeReader.cs
+++ RimeLibLite/IO/ZlibRimeReader.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using ICSharpCode.SharpZipLib.Zip.Compression;
 using ICSharpCode.SharpZipLib.Zip.Compression.Streams;
 using RimeLib.IO.Conversion;
 
@@ -57,7 +58,7 @@ namespace RimeLib.IO
             // If the original size is not equal to the compressed size then we need to wrap in an inflater stream.
             // Otherwise, we just return as-is.
             if (p_Segment.CompressedSize != p_Segment.OriginalSize)
-                s_Stream = new InflaterInputStream(s_Stream);
+                s_Stream = new InflaterInputStream(s_Stream, new Inflater(), (int)p_Segment.OriginalSize);
 
             return s_Stream;
         }
@@ -98,7 +99,51 @@ namespace RimeLib.IO
                 return Position;
             }
             
-            throw new NotSupportedException();
+            long s_AbsoluteOffset = p_Origin switch
+            {
+                SeekOrigin.Begin => p_Offset,
+                SeekOrigin.Current => Position + p_Offset,
+                SeekOrigin.End => m_OriginalSize - p_Offset, // TODO: is this minus or plus
+            };
+            
+            if (s_AbsoluteOffset == Position)
+                return Position;
+            
+            if (s_AbsoluteOffset > m_OriginalSize)
+                throw new InvalidDataException();
+            
+            (int, ZlibSegment)? s_TargetSegment = null;
+            
+            long s_SegmentDecompresedOffset = 0;
+            for (var s_I = 0; s_I < m_Segments.Count; s_I++)
+            {
+                var s_Segment = m_Segments[s_I];
+                
+                if (s_AbsoluteOffset >= s_SegmentDecompresedOffset &&
+                    s_AbsoluteOffset < s_SegmentDecompresedOffset + s_Segment.OriginalSize)
+                {
+                    s_TargetSegment = (s_I, s_Segment);
+                    break;
+                }
+                
+                s_SegmentDecompresedOffset += s_Segment.OriginalSize;
+            }
+            if (s_TargetSegment == null)
+                throw new Exception();
+
+            // Seek to the next segment.
+            m_CurrentSegment = s_TargetSegment.Value.Item1;
+            m_SegmentStream.Dispose();
+            m_SegmentStream = GetStreamForSegment(m_Segments[m_CurrentSegment]);
+            var s_OffsetInStream = s_SegmentDecompresedOffset - s_SegmentDecompresedOffset;
+            if (s_OffsetInStream > 0)
+            {
+                var s_Buffer = new byte[s_OffsetInStream];
+                m_SegmentStream.Read(s_Buffer, 0, (int)s_OffsetInStream);
+            }
+            m_CurrentPosition = s_AbsoluteOffset;
+            
+            return Position;
         }
 
         protected override int ReadInternal(byte[] p_Data, int p_Index, int p_Count)
@@ -107,7 +152,7 @@ namespace RimeLib.IO
             
             while (s_Remaining > 0)
             {
-                var s_ReadData = m_SegmentStream.Read(p_Data, p_Index + (p_Count - s_Remaining), s_Remaining);
+                var s_ReadData = m_SegmentStream.Read(p_Data, p_Index + (p_Count - s_Remaining), System.Math.Min(s_Remaining, (int)m_Segments[m_CurrentSegment].OriginalSize));
                 
                 m_CurrentPosition += s_ReadData;
                 s_Remaining -= s_ReadData;
