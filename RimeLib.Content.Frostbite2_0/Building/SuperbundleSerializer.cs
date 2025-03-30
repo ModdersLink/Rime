da8e21fbd2a15d0a4d22d4702a342ee33c69ce0c
FlashHit <56718716+FlashHit@users.noreply.github.com>
2025-03-30 22:23:03 +0200

    Force magicSalt

--- RimeLib.Content.Frostbite2_0/Building/SuperbundleSerializer.cs
+++ RimeLib.Content.Frostbite2_0/Building/SuperbundleSerializer.cs
@@ -56,6 +56,7 @@ namespace RimeLib.Content.Frostbite2_0.Building
                     // Create a new DbObject
                     var s_CasBundle = new CasBundle
                     {
+                        MagicSalt = 2047996331, // TODO: figure out how to set this the proper way.
                         Path = p_Descriptor.BundleName.ToLowerInvariant(),
                         ResourceEntries = new CasBundle.Resource[p_Descriptor.Resources.Count],
                         EbxEntries = new CasBundle.Ebx[p_Descriptor.Partitions.Count],
