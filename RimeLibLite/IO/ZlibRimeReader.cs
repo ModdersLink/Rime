7bc88815a8782da68d384742a96c2359f4499a53
FlashHit <56718716+FlashHit@users.noreply.github.com>
2026-04-06 19:23:31 +0200

    Fix ZlibRimeReader seek
    Co-Authored-By: txt <4959042+txt231@users.noreply.github.com>

--- RimeLibLite/IO/ZlibRimeReader.cs
+++ RimeLibLite/IO/ZlibRimeReader.cs
@@ -32,7 +32,7 @@ namespace RimeLib.IO
 
         protected readonly List<ZlibSegment> m_Segments = new List<ZlibSegment>();
 
-        public ZlibRimeReader(RimeReader p_Reader, bool p_ShouldDispose = true) : 
+        public ZlibRimeReader(RimeReader p_Reader, bool p_ShouldDispose = true) :
             base(p_Reader, p_Reader.Endianness, p_ShouldDispose)
         {
             // If the user has specified there are sizes here, we should parse them.
@@ -53,8 +53,8 @@ namespace RimeLib.IO
             // Seek to where we need to be.
             BaseStream.Seek(p_Segment.Offset + 8, SeekOrigin.Begin);
 
-            Stream s_Stream = new LimitedRimeReader((RimeReader) BaseStream, p_Segment.CompressedSize, false);
-                
+            Stream s_Stream = new LimitedRimeReader((RimeReader)BaseStream, p_Segment.CompressedSize, false);
+
             // If the original size is not equal to the compressed size then we need to wrap in an inflater stream.
             // Otherwise, we just return as-is.
             if (p_Segment.CompressedSize != p_Segment.OriginalSize)
@@ -83,8 +83,8 @@ namespace RimeLib.IO
                 var s_Segment = new ZlibSegment
                 {
                     Offset = BaseStream.Position,
-                    OriginalSize = EndianBitConverter.Big.ToInt32(((RimeReader) BaseStream).ReadBytes(4), 0),
-                    CompressedSize = EndianBitConverter.Big.ToInt32(((RimeReader) BaseStream).ReadBytes(4), 0),
+                    OriginalSize = EndianBitConverter.Big.ToInt32(((RimeReader)BaseStream).ReadBytes(4), 0),
+                    CompressedSize = EndianBitConverter.Big.ToInt32(((RimeReader)BaseStream).ReadBytes(4), 0),
                 };
 
                 s_Segment.LeftData = s_Segment.CompressedSize;
@@ -103,75 +103,103 @@ namespace RimeLib.IO
         {
             if (p_Origin == SeekOrigin.Current && p_Offset > 0)
             {
-                ReadBytes((int) p_Offset);
+                ReadBytes((int)p_Offset);
                 return Position;
             }
-            
+
             long s_AbsoluteOffset = p_Origin switch
             {
                 SeekOrigin.Begin => p_Offset,
                 SeekOrigin.Current => Position + p_Offset,
                 SeekOrigin.End => m_OriginalSize - p_Offset, // TODO: is this minus or plus
             };
-            
+
             if (s_AbsoluteOffset == Position)
                 return Position;
-            
+
             if (s_AbsoluteOffset > m_OriginalSize)
                 throw new InvalidDataException();
-            
+
+            // if (s_AbsoluteOffset > Position)
+            // {
+            //     // TODO: THis is worse for files with multiple segments.
+            //     ReadBytes((int) (s_AbsoluteOffset - Position));
+            //     return Position;
+            // }
+
             (int, ZlibSegment)? s_TargetSegment = null;
-            
+
             long s_SegmentDecompresedOffset = 0;
             for (var s_I = 0; s_I < m_Segments.Count; s_I++)
             {
                 var s_Segment = m_Segments[s_I];
-                
+
                 if (s_AbsoluteOffset >= s_SegmentDecompresedOffset &&
                     s_AbsoluteOffset < s_SegmentDecompresedOffset + s_Segment.OriginalSize)
                 {
                     s_TargetSegment = (s_I, s_Segment);
                     break;
                 }
-                
+
                 s_SegmentDecompresedOffset += s_Segment.OriginalSize;
             }
+
+            if (s_TargetSegment == null && s_AbsoluteOffset == s_SegmentDecompresedOffset)
+                s_TargetSegment = (m_Segments.Count - 1, m_Segments[^1]);
+
+            // Should we crash here? if we set position to end of buffer this will trigger.
             if (s_TargetSegment == null)
                 throw new Exception();
 
-            // Seek to the next segment.
-            m_CurrentSegment = s_TargetSegment.Value.Item1;
-            m_SegmentStream.Dispose();
-            m_SegmentStream = GetStreamForSegment(m_Segments[m_CurrentSegment]);
-            var s_OffsetInStream = s_SegmentDecompresedOffset - s_SegmentDecompresedOffset;
-            if (s_OffsetInStream > 0)
+
+            var s_OffsetInStream = s_AbsoluteOffset - s_SegmentDecompresedOffset;
+            if (s_OffsetInStream < 0)
+                throw new NotImplementedException();
+
+            // NOTE: zlib streame or whatever doesnt support seeks. 
+            // if (s_TargetSegment.Value.Item1 == m_CurrentSegment)
+
+            if (s_TargetSegment.Value.Item1 == m_CurrentSegment && s_AbsoluteOffset > m_CurrentPosition)
             {
-                var s_Buffer = new byte[s_OffsetInStream];
-                m_SegmentStream.Read(s_Buffer, 0, (int)s_OffsetInStream);
+                var s_SizeToRead = s_AbsoluteOffset - m_CurrentPosition;
+
+                var s_JunkData = new byte[s_SizeToRead];
+                m_SegmentStream.Read(s_JunkData, 0, (int)s_SizeToRead);
+            }
+            else
+            {
+                // Seek to the next segment.
+                m_CurrentSegment = s_TargetSegment.Value.Item1;
+                m_SegmentStream.Dispose();
+                m_SegmentStream = GetStreamForSegment(m_Segments[m_CurrentSegment]);
+
+
+                var s_JunkData = new byte[s_OffsetInStream];
+                m_SegmentStream.Read(s_JunkData, 0, (int)s_OffsetInStream);
             }
             m_CurrentPosition = s_AbsoluteOffset;
-            
+
             return Position;
         }
 
         protected override int ReadInternal(byte[] p_Data, int p_Index, int p_Count)
         {
             var s_Remaining = p_Count;
-            
+
             while (s_Remaining > 0)
             {
                 var s_ReadData = m_SegmentStream.Read(p_Data, p_Index + (p_Count - s_Remaining), System.Math.Min(s_Remaining, (int)m_Segments[m_CurrentSegment].OriginalSize));
-                
+
                 m_CurrentPosition += s_ReadData;
                 s_Remaining -= s_ReadData;
 
                 // If we didn't read enough data we need to move to the next segment.
-                if (s_Remaining == 0) 
+                if (s_Remaining == 0)
                     continue;
 
                 if (s_Remaining < 0)
                     throw new Exception("Read more data than we should have. Something has definitely gone wrong.");
-                
+
                 m_SegmentStream.Dispose();
 
                 // Seek to the next segment.
